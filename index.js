e8f18200d2197c6000903c48c563f4ee6f62e540
webafsanakeya <webafsakeya@gmail.com>
2025-06-15 17:16:47 +0600

    use cookie parser middleware and set token to the cookie,verify token

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
+const jwt = require("jsonwebtoken");
+const cookieParser = require("cookie-parser");
 const app = express();
 const port = process.env.PORT || 3000;
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: ['http://localhost:5173'],
+  credentials: true
+}));
 app.use(express.json());
+
+app.use(cookieParser(process.env.COOKIE_SECRET));
+
+const logger = (req, res, next) =>{
+  console.log('inside the logger middleware');
+  next();
+}
+
+const verifyToken = (req, res, next) =>{
+  const token = req?.cookies?.token;
+  console.log('cookie in the middleware', token);
+  if(!token){
+    return res.status(401).send({message: 'unauthorized access'})
+  }
+  // verify token
+  jwt.verify(token, process.env.JWT_ACCESS_SECRET, (err, decoded) =>{
+    if(err){
+      return res.status(401).send({message: 'unauthorized access'})
+    }
+    req.decoded = decoded;
+    next();
+  })
+
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.3g5ecwq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -28,6 +57,21 @@ async function run() {
     const itemsCollection = client.db("findLost").collection("items");
     const recoveriesCollection = client.db("findLost").collection("recoveries");
 
+    // jwt token related api
+
+    app.post('/jwt', async(req, res)=>{
+      const userData = req.body;
+      const token = jwt.sign(userData, process.env.JWT_ACCESS_SECRET, 
+        {expiresIn: '1d'})
+
+        // set token in the cookies
+        res.cookie('token', token, {
+          httpOnly: true,
+          secure: false
+        })
+      res.send({success: true})
+    })
+    
     // items api
     app.get("/items", async (req, res) => {
       const email = req.query.email;
@@ -40,17 +84,19 @@ async function run() {
       res.send(result);
     });
 
-     app.get('/items/recoveries', async(req, res) =>{
+    app.get("/items/recoveries", async (req, res) => {
       const email = req.query.email;
-      const query = {email: email};
+      const query = { email: email };
       const items = await itemsCollection.find(query).toArray();
-      for(const item of items){
-        const recoveryQuery ={itemId: item._id.toString()}
-        const recovery_count = await recoveriesCollection.countDocuments(recoveryQuery)
+      for (const item of items) {
+        const recoveryQuery = { itemId: item._id.toString() };
+        const recovery_count = await recoveriesCollection.countDocuments(
+          recoveryQuery
+        );
         item.recovery_count = recovery_count;
       }
       res.send(items);
-    })
+    });
 
     app.get("/items/:id", async (req, res) => {
       const id = req.params.id;
@@ -66,15 +112,17 @@ async function run() {
       res.send(result);
     });
 
-   
-
     // item recoveries related api
 
-    app.get("/recoveries", async (req, res) => {
+    app.get("/recoveries", logger, verifyToken, async (req, res) => {
       const email = req.query.email;
-      if (!email) {
-        return res.status(400).send({ error: "Email is required" });
+     
+      
+      // console.log('inside recoveries api', req.cookies);
+      if(email !== req.decoded.email ){
+        return res.status(403).send({message: 'forbidden access'})
       }
+      
       const query = {
         "recoveredBy.email": email,
       };
@@ -114,25 +162,25 @@ async function run() {
 
     app.post("/recoveries", async (req, res) => {
       const recoveryData = req.body;
-      
-  if (!recoveryData.itemId) {
-    return res.status(400).send({ message: "Missing itemId" });
-  }
+
+      if (!recoveryData.itemId) {
+        return res.status(400).send({ message: "Missing itemId" });
+      }
       const result = await recoveriesCollection.insertOne(recoveryData);
       res.send(result);
     });
 
-    app.patch('/recoveries/:id', async(req, res) =>{
+    app.patch("/recoveries/:id", async (req, res) => {
       const id = req.params.id;
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) };
       const updatedDoc = {
         $set: {
-          status: req.body.status
-        }
-      }
-      const result = await recoveriesCollection.updateOne(filter, updatedDoc)
+          status: req.body.status,
+        },
+      };
+      const result = await recoveriesCollection.updateOne(filter, updatedDoc);
       res.send(result);
-    })
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
